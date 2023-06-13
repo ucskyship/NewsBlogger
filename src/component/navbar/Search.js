6dbdbc28e46e97a9bf18f5b2290d9a06392c9fc9
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-06-13 19:01:01 +0100

    dassasd

--- src/component/navbar/Search.js
+++ src/component/navbar/Search.js
@@ -1,22 +1,44 @@
-import React, { useSate } from "react";
-import data from "../newsCard/NewsList";
-
+import React, { useState, useContext } from "react";
+import { ArticlesContext } from "../../context";
+import icon from "../../assets/searchIcon.svg";
+import "./SearchBar.css";
 const Search = () => {
-    const [filter, setFilter] = useSate("");
-    const searchText = (event) => {
-        setFilter(event.target.value);
-    }
-    let dataSearch = data.filter(item => {
-        return Object.keys(item).some(key => {
-            item[key].toString().toLowerCase().includes(filter.toString().toLowerCase())
-        })
-    })
+  const { setArticles, articles, allArticles } = useContext(ArticlesContext);
+  const [value, setValue] = useState("");
 
-    return(
-        <div>
-            <Search/>
-        </div>
-    )
-}
+  const performSearch = (e) => {
+    setValue(e.target.value);
+    console.log(e.target.value);
+    if (e.target.value === "") {
+      console.log("all arts");
+      console.log(allArticles);
+      setArticles(allArticles);
+      return;
+    }
+    const filterArts = articles.filter((article) =>
+      article.title.toLowerCase().includes(value.toLowerCase())
+    );
+    console.log("filtered arts", filterArts);
+
+    setArticles(filterArts);
+  };
+  return (
+    <div className={"container"}>
+      <input
+        className={"inputField"}
+        type={"text"}
+        placeholder={"Search here..."}
+        value={value}
+        onChange={(e) => performSearch(e)}
+      />
+      <img
+        //   onClick={performSearch}
+        className={"searchIcon"}
+        src={icon}
+        alt={"icon"}
+      />
+    </div>
+  );
+};
 
 export default Search;
