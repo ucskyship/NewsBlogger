872ce157012a1fd23ba58b62e21d719fcb6bcd26
ucskyship <chat.with.ucjhay@gmail.com>
2023-06-20 00:40:54 +0100

    latest

--- src/component/newsCard/NewsList.js
+++ src/component/newsCard/NewsList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import NewsCard from "./NewsCard";
 import "./Card.css";
 import "../paginate/Paginate";
